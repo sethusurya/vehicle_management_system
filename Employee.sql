a56784e4893091bf2a502c78fee87157bc39803c
SaiswarnaR <90537859+SaiswarnaR@users.noreply.github.com>
2022-04-23 14:46:41 -0400

    Add files via upload
    Creation of sequence for EMPLOYEE_ID

---
+++ Employee.sql
@@ -0,0 +1,7 @@
+- SEQUENCE FOR EMPLOYEE_ID
+---------------------------------------------------------
+
+    CREATE SEQUENCE EMPLOYEE _ID_SEQ
+         START WITH 1
+         INCREMENT BY 1
+         ORDER;
