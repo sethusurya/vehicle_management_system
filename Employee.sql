649e3c4f85238c2cb8edc1f3e858569c7835d562
SaiswarnaR <90537859+SaiswarnaR@users.noreply.github.com>
2022-04-23 14:48:02 -0400

    Command for Employee Table creation

--- Employee.sql
+++ Employee.sql
@@ -5,3 +5,17 @@
          START WITH 1
          INCREMENT BY 1
          ORDER;
+
+--  DDL for Table EMPLOYEE
+--------------------------------------------------------
+
+  CREATE TABLE CAR   
+   (	  EMPLOYEE_ID   NUMBER(25,0), 
+	  EMP_FIRST_NAME   VARCHAR2(60)  , 
+	  EMP_LAST_NAME VARCHAR2(60)  , 
+	  EMP_EMAIL  VARCHAR2(255)  ,
+	  EMP_PASSWORD  VARCHAR2(255)  , 
+	  DATE_OF_JOINING  DATE , 
+	  ADDRESS_ID VARCHAR(255),
+	  ROLE_ID VARCHAR(255) 
+   )
