4031c8c21c06b3f9b7462146d176ccd2089012f9
NoordeepSinghGill <90423768+NoordeepSinghGill@users.noreply.github.com>
2022-04-23 16:39:19 -0400

    role package added

---
+++ validations/role_validations.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE EDITIONABLE PACKAGE PCKG_ROLE  AS 
+    PROCEDURE INSERT_ROLE(
+        vROLE_NAME IN ROLE.ROLE_NAME%type
+        );
+END PCKG_ROLE;
+/
+
+CREATE OR REPLACE EDITIONABLE PACKAGE BODY PCKG_ROLE  AS 
+    PROCEDURE INSERT_ROLE(
+        vROLE_NAME IN ROLE.ROLE_NAME%type
+        ) AS     
+        
+        ex_INVALID_ROLE_NAME EXCEPTION; 
+        DB_ROLE_NAME NUMBER(38);
+    
+    BEGIN
+        
+        BEGIN
+        select COUNT(ROLE_NAME) INTO DB_ROLE_NAME FROM ROLE WHERE ROLE_NAME = vROLE_NAME;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+            DB_ROLE_NAME := 0;
+        END;
+        
+        if vROLE_NAME is NULL or LENGTH(TRIM(vROLE_NAME)) IS NULL then
+            raise ex_INVALID_ROLE_NAME;
+        end if;
+        
+        IF DB_ROLE_NAME = 0 AND UPPER(vROLE_NAME) in ('OWNER','DEVELOPER','TESTER', 'ANALYST') THEN
+            INSERT INTO ROLE(ROLE_ID, ROLE_NAME) VALUES (ROLE_ID_SEQ.NEXTVAL, vROLE_NAME);
+        END IF; 
+        
+        EXCEPTION
+            when ex_INVALID_ROLE_NAME then
+                dbms_output.put_line('Role name is invalid !!!');
+    END INSERT_ROLE;
+END PCKG_ROLE; 
+/
+
+EXECUTE INSERT_ROLE('DEVELOPER');
+SELECT * FROM ROLE;
