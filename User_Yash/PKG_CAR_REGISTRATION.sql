3ccdfdec4cf8010a3ef7e6889104671406a549eb
yj199522 <jain.yash3@northeastern.edu>
2022-04-26 22:29:51 -0400

    update car register

--- User_Yash/PKG_CAR_REGISTRATION.sql
+++ User_Yash/PKG_CAR_REGISTRATION.sql
@@ -1,6 +1,19 @@
 set serveroutput on;
 
 CREATE OR REPLACE EDITIONABLE PACKAGE PKG_CAR_REGISTER   AS 
+    PROCEDURE BODY_CAR_REGISTER_DATA(
+        vCAR_REGISTER_ID IN CAR_REGISTRATION.CAR_REGISTER_ID%type,
+        vCAR_NAME IN CAR_REGISTRATION.CAR_NAME%type, 
+        vCAR_COLOR IN CAR_REGISTRATION.CAR_COLOR%type,
+        vCAR_COMPANY IN CAR_REGISTRATION.CAR_COMPANY%type,
+        vYEAR_OF_MANUFACTURE IN NUMBER,
+        vREGISTRATION_DATE IN CAR_REGISTRATION.REGISTRATION_DATE%type,
+        vFUEL_TYPE IN CAR_REGISTRATION.FUEL_TYPE%type,
+        vTRANSMISSION_TYPE IN CAR_REGISTRATION.TRANSMISSION_TYPE%type,
+        vNO_OF_SEATS IN NUMBER,
+        vCAR_CATEGORY_NAME IN CAR_CATEGORY.CAR_CATEGORY_NAME%type,
+        vEMP_EMAIL IN EMPLOYEE.EMP_EMAIL%type
+    ); 
     PROCEDURE BODY_CAR_REGISTER( 
         vCAR_REGISTER_ID IN CAR_REGISTRATION.CAR_REGISTER_ID%type,
         vCAR_NAME IN CAR_REGISTRATION.CAR_NAME%type, 
@@ -49,17 +62,16 @@ CREATE OR REPLACE EDITIONABLE PACKAGE PKG_CAR_REGISTER   AS
     
     PROCEDURE BODY_CAR_REGISTER_UPDATE_TABLE(
         vCAR_REGISTER_ID IN CAR_REGISTRATION.CAR_REGISTER_ID%type,
-        vCAR_NAME IN OUT CAR_REGISTRATION.CAR_NAME%type, 
-        vCAR_COLOR IN OUT CAR_REGISTRATION.CAR_COLOR%type,
-        vCAR_COMPANY IN OUT CAR_REGISTRATION.CAR_COMPANY%type,
-        vYEAR_OF_MANUFACTURE IN OUT NUMBER,
-        vREGISTRATION_DATE IN OUT CAR_REGISTRATION.REGISTRATION_DATE%type,
-        vFUEL_TYPE IN OUT CAR_REGISTRATION.FUEL_TYPE%type,
-        vTRANSMISSION_TYPE IN OUT CAR_REGISTRATION.TRANSMISSION_TYPE%type,
-        vNO_OF_SEATS IN OUT NUMBER,
-        vCAR_CATEGORY_ID IN CAR_REGISTRATION.CAR_CATEGORY_ID%type,
-        vEMPLOYEE_ID IN CAR_REGISTRATION.EMPLOYEE_ID%type,
-        vADDRESS_ID IN CAR_REGISTRATION.ADDRESS_ID%type
+        uCAR_NAME IN CAR_REGISTRATION.CAR_NAME%type, 
+        uCAR_COLOR IN CAR_REGISTRATION.CAR_COLOR%type,
+        uCAR_COMPANY IN CAR_REGISTRATION.CAR_COMPANY%type,
+        uYEAR_OF_MANUFACTURE IN NUMBER,
+        uREGISTRATION_DATE IN CAR_REGISTRATION.REGISTRATION_DATE%type,
+        uFUEL_TYPE IN CAR_REGISTRATION.FUEL_TYPE%type,
+        uTRANSMISSION_TYPE IN CAR_REGISTRATION.TRANSMISSION_TYPE%type,
+        uNO_OF_SEATS IN NUMBER,
+        uCAR_CATEGORY_NAME IN CAR_CATEGORY.CAR_CATEGORY_NAME%type,
+        vEMP_EMAIL IN EMPLOYEE.EMP_EMAIL%type
     );
     
      PROCEDURE BODY_CAR_REGISTER_DELETE_DATA(
@@ -75,6 +87,57 @@ END PKG_CAR_REGISTER;
 /
 
 create or replace PACKAGE BODY PKG_CAR_REGISTER AS
+     PROCEDURE BODY_CAR_REGISTER_DATA(
+        vCAR_REGISTER_ID IN CAR_REGISTRATION.CAR_REGISTER_ID%type,
+        vCAR_NAME IN CAR_REGISTRATION.CAR_NAME%type, 
+        vCAR_COLOR IN CAR_REGISTRATION.CAR_COLOR%type,
+        vCAR_COMPANY IN CAR_REGISTRATION.CAR_COMPANY%type,
+        vYEAR_OF_MANUFACTURE IN NUMBER,
+        vREGISTRATION_DATE IN CAR_REGISTRATION.REGISTRATION_DATE%type,
+        vFUEL_TYPE IN CAR_REGISTRATION.FUEL_TYPE%type,
+        vTRANSMISSION_TYPE IN CAR_REGISTRATION.TRANSMISSION_TYPE%type,
+        vNO_OF_SEATS IN NUMBER,
+        vCAR_CATEGORY_NAME IN CAR_CATEGORY.CAR_CATEGORY_NAME%type,
+        vEMP_EMAIL IN EMPLOYEE.EMP_EMAIL%type
+    ) AS 
+    INVALID_EMPLOYEE_ID EXCEPTION;
+    INVALID_CAR_REGISTER_ID_PRESENT EXCEPTION;
+    ID_CHECK Number;
+    vEMPLOYEE_ID CAR_REGISTRATION.EMPLOYEE_ID%type;
+    vADDRESS_ID CAR_REGISTRATION.ADDRESS_ID%type;
+    vCAR_CATEGORY_ID CAR_CATEGORY.CAR_CATEGORY_ID%type;
+    BEGIN
+      if vEMP_EMAIL is NULL or LENGTH(TRIM(vEMP_EMAIL)) is NULL or NOT (TRIM(vEMP_EMAIL)) LIKE '%@_%._%' then
+            raise INVALID_EMPLOYEE_ID;
+        end if;
+
+        begin
+            select Count(*) into ID_CHECK from EMPLOYEE where EMP_EMAIL = UPPER(vEMP_EMAIL);
+            if(ID_CHECK = 0) then 
+                raise INVALID_EMPLOYEE_ID;
+            end if;
+            select EMPLOYEE_ID into vEMPLOYEE_ID from EMPLOYEE where EMP_EMAIL = UPPER(vEMP_EMAIL);
+            select ADDRESS_ID into vADDRESS_ID from EMPLOYEE where EMP_EMAIL = UPPER(vEMP_EMAIL); 
+        end;
+        begin
+            PCKG_CAR_CATEGORY.CHECK_AND_RETURN_ID(vCAR_CATEGORY_NAME, vCAR_CATEGORY_ID);
+            dbms_output.put_line(vCAR_CATEGORY_ID);
+        end;
+        begin
+            select Count(*) into ID_CHECK from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+            IF ID_CHECK > 0 THEN
+                raise INVALID_CAR_REGISTER_ID_PRESENT;
+            END IF;
+        end;
+            BODY_CAR_REGISTER(UPPER(vCAR_REGISTER_ID), UPPER(vCAR_NAME), UPPER(vCAR_COLOR), UPPER(vCAR_COMPANY), to_number(vYEAR_OF_MANUFACTURE), vREGISTRATION_DATE, UPPER(vFUEL_TYPE), UPPER(vTRANSMISSION_TYPE), to_number(vNO_OF_SEATS), UPPER(vCAR_CATEGORY_ID), UPPER(vEMPLOYEE_ID), UPPER(vADDRESS_ID));
+    EXCEPTION
+          when INVALID_EMPLOYEE_ID then
+            dbms_output.put_line('Invalid employee email !!!');
+          when INVALID_CAR_REGISTER_ID_PRESENT then
+            dbms_output.put_line('CAR REGISTER ID already present !!!');
+
+    END BODY_CAR_REGISTER_DATA;
+
     PROCEDURE BODY_CAR_REGISTER(
         vCAR_REGISTER_ID IN CAR_REGISTRATION.CAR_REGISTER_ID%type,
         vCAR_NAME IN CAR_REGISTRATION.CAR_NAME%type, 
@@ -116,7 +179,6 @@ create or replace PACKAGE BODY PKG_CAR_REGISTER AS
         v_date date;
         ID_CHECK NUMBER;
         INVALID_CAR_REGISTER_ID EXCEPTION;
-        INVALID_CAR_REGISTER_ID_PRESENT EXCEPTION;
         INVALID_CAR_NAME EXCEPTION;
         INVALID_CAR_COLOR EXCEPTION;
         INVALID_CAR_COMPANY EXCEPTION;
@@ -212,24 +274,14 @@ create or replace PACKAGE BODY PKG_CAR_REGISTER AS
         end if;
 
         begin
-            select Count(*) into ID_CHECK from ADDRESS where ADDRESS_ID = UPPER(vADDRESS_ID) AND ADDRESS_TYPE = 'PARKING';
+            select Count(*) into ID_CHECK from ADDRESS where ADDRESS_ID = UPPER(vADDRESS_ID) AND ADDRESS_TYPE = 'EMPLOYEE';
              if(ID_CHECK = 0) then 
                 raise INVALID_ADDRESS_ID;
             end if;
         end;
-
-        begin
-            select Count(*) into ID_CHECK from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
-            IF ID_CHECK > 0 THEN
-                raise INVALID_CAR_REGISTER_ID_PRESENT;
-            END IF;
-        end;
         RETURN 'YES';
 
     EXCEPTION
-        when INVALID_CAR_REGISTER_ID_PRESENT then
-            dbms_output.put_line('CAR REGISTER ID already present !!!');
-            RETURN 'NO';
         when INVALID_CAR_REGISTER_ID then
             dbms_output.put_line('CAR REGISTER ID is invalid !!!');
             RETURN 'NO';
@@ -292,7 +344,7 @@ create or replace PACKAGE BODY PKG_CAR_REGISTER AS
     RETURN NUMBER AS
     counts NUMBER;
     BEGIN
-        select Count(*) into counts FROM CAR_REGISTRATION where vCAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        select Count(*) into counts FROM CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
         RETURN counts;
     END BODY_CAR_REGISTER_ID_CHECK; 
     
@@ -306,62 +358,121 @@ create or replace PACKAGE BODY PKG_CAR_REGISTER AS
              dbms_output.put_line('Invalid CAR_REGISTER ID !!!');
         end if;
         Delete from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        dbms_output.put_line('Data deleted Successfully !!!');
     END BODY_CAR_REGISTER_DELETE_DATA;
     
     PROCEDURE BODY_CAR_REGISTER_UPDATE_TABLE(
         vCAR_REGISTER_ID IN CAR_REGISTRATION.CAR_REGISTER_ID%type,
-        vCAR_NAME IN OUT CAR_REGISTRATION.CAR_NAME%type, 
-        vCAR_COLOR IN OUT CAR_REGISTRATION.CAR_COLOR%type,
-        vCAR_COMPANY IN OUT CAR_REGISTRATION.CAR_COMPANY%type,
-        vYEAR_OF_MANUFACTURE IN OUT NUMBER,
-        vREGISTRATION_DATE IN OUT CAR_REGISTRATION.REGISTRATION_DATE%type,
-        vFUEL_TYPE IN OUT CAR_REGISTRATION.FUEL_TYPE%type,
-        vTRANSMISSION_TYPE IN OUT CAR_REGISTRATION.TRANSMISSION_TYPE%type,
-        vNO_OF_SEATS IN OUT NUMBER,
-        vCAR_CATEGORY_ID IN CAR_REGISTRATION.CAR_CATEGORY_ID%type,
-        vEMPLOYEE_ID IN CAR_REGISTRATION.EMPLOYEE_ID%type,
-        vADDRESS_ID IN CAR_REGISTRATION.ADDRESS_ID%type
+        uCAR_NAME IN CAR_REGISTRATION.CAR_NAME%type, 
+        uCAR_COLOR IN CAR_REGISTRATION.CAR_COLOR%type,
+        uCAR_COMPANY IN CAR_REGISTRATION.CAR_COMPANY%type,
+        uYEAR_OF_MANUFACTURE IN NUMBER,
+        uREGISTRATION_DATE IN CAR_REGISTRATION.REGISTRATION_DATE%type,
+        uFUEL_TYPE IN CAR_REGISTRATION.FUEL_TYPE%type,
+        uTRANSMISSION_TYPE IN CAR_REGISTRATION.TRANSMISSION_TYPE%type,
+        uNO_OF_SEATS IN NUMBER,
+        uCAR_CATEGORY_NAME IN CAR_CATEGORY.CAR_CATEGORY_NAME%type,
+        vEMP_EMAIL IN EMPLOYEE.EMP_EMAIL%type
     ) AS
     Conditions VARCHAR2(244);
     temp_car_id Number;
+    ID_CHECK Number;
+    vCAR_NAME CAR_REGISTRATION.CAR_NAME%type;
+    vCAR_COLOR CAR_REGISTRATION.CAR_COLOR%type;
+    vCAR_COMPANY CAR_REGISTRATION.CAR_COMPANY%type;
+    vYEAR_OF_MANUFACTURE NUMBER;
+    vREGISTRATION_DATE CAR_REGISTRATION.REGISTRATION_DATE%type;
+    vFUEL_TYPE CAR_REGISTRATION.FUEL_TYPE%type;
+    vTRANSMISSION_TYPE CAR_REGISTRATION.TRANSMISSION_TYPE%type;
+    vNO_OF_SEATS NUMBER;
+    vEMPLOYEE_ID CAR_REGISTRATION.EMPLOYEE_ID%type;
+    vADDRESS_ID CAR_REGISTRATION.ADDRESS_ID%type;
+    vCAR_CATEGORY_ID CAR_CATEGORY.CAR_CATEGORY_ID%type;
+    INVALID_EMPLOYEE_ID EXCEPTION;
+    INVALID_CAR_REGISTER_ID EXCEPTION;
     BEGIN
+        if vCAR_REGISTER_ID is NULL or LENGTH(TRIM(vCAR_REGISTER_ID)) is NULL then
+            raise INVALID_CAR_REGISTER_ID;
+        end if;
         Select BODY_CAR_REGISTER_ID_CHECK(vCAR_REGISTER_ID) INTO temp_car_id from dual;
         if Not(temp_car_id > 0) then
-             dbms_output.put_line('Invalid CAR_REGISTER ID !!!');
+             raise INVALID_CAR_REGISTER_ID;
         end if;
         
-        if vCAR_NAME is NULL or LENGTH(TRIM(vCAR_NAME)) is NULL then
+        if uCAR_CATEGORY_NAME is NULL or LENGTH(TRIM(uCAR_CATEGORY_NAME)) is NULL then
+            Select CAR_CATEGORY_ID into vCAR_CATEGORY_ID from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            begin
+            PCKG_CAR_CATEGORY.CHECK_AND_RETURN_ID(uCAR_CATEGORY_NAME, vCAR_CATEGORY_ID);
+            dbms_output.put_line(vCAR_CATEGORY_ID);
+        end;
+        end if;
+        
+        if vEMP_EMAIL is NULL or LENGTH(TRIM(vEMP_EMAIL)) is NULL then
+            Select EMPLOYEE_ID into vEMPLOYEE_ID from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            if vEMP_EMAIL is NULL or LENGTH(TRIM(vEMP_EMAIL)) is NULL or NOT (TRIM(vEMP_EMAIL)) LIKE '%@_%._%' then
+                raise INVALID_EMPLOYEE_ID;
+            end if;
+            begin
+                select Count(*) into ID_CHECK from EMPLOYEE where EMP_EMAIL = UPPER(vEMP_EMAIL);
+                if(ID_CHECK = 0) then 
+                    raise INVALID_EMPLOYEE_ID;
+                end if;
+                select EMPLOYEE_ID into vEMPLOYEE_ID from EMPLOYEE where EMP_EMAIL = UPPER(vEMP_EMAIL);
+                select ADDRESS_ID into vADDRESS_ID from EMPLOYEE where EMP_EMAIL = UPPER(vEMP_EMAIL); 
+            end;
+        end if;
+        
+        if uCAR_NAME is NULL or LENGTH(TRIM(uCAR_NAME)) is NULL then
             Select CAR_NAME into vCAR_NAME from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vCAR_NAME:= uCAR_NAME;
+            
         end if;
-
-        if vCAR_COLOR is NULL or  LENGTH(vCAR_COLOR) is NULL then
+        
+        if uCAR_COLOR is NULL or  LENGTH(uCAR_COLOR) is NULL then
             Select CAR_COLOR into vCAR_COLOR from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vCAR_COLOR:= uCAR_COLOR;
         end if;
 
-        if vCAR_COMPANY is NULL or LENGTH(TRIM(vCAR_COMPANY)) is NULL then
+        if uCAR_COMPANY is NULL or LENGTH(TRIM(uCAR_COMPANY)) is NULL then
             Select CAR_COMPANY into vCAR_COMPANY from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vCAR_COMPANY:= uCAR_COMPANY;
         end if; 
 
-        if vYEAR_OF_MANUFACTURE is NULL or to_char(LENGTH(vYEAR_OF_MANUFACTURE)) is NULL then
+        if uYEAR_OF_MANUFACTURE is NULL or to_char(LENGTH(uYEAR_OF_MANUFACTURE)) is NULL then
             Select YEAR_OF_MANUFACTURE into vYEAR_OF_MANUFACTURE from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vYEAR_OF_MANUFACTURE:= uYEAR_OF_MANUFACTURE;
         end if;
         
-        if vFUEL_TYPE is NULL or LENGTH(TRIM(vFUEL_TYPE)) is NULL then
+        if uFUEL_TYPE is NULL or LENGTH(TRIM(uFUEL_TYPE)) is NULL then
             Select FUEL_TYPE into vFUEL_TYPE from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vFUEL_TYPE:= uFUEL_TYPE;
         end if;  
 
-        if vTRANSMISSION_TYPE is NULL or  LENGTH(vTRANSMISSION_TYPE) is NULL then
+        if uTRANSMISSION_TYPE is NULL or  LENGTH(uTRANSMISSION_TYPE) is NULL then
             Select TRANSMISSION_TYPE into vTRANSMISSION_TYPE from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vTRANSMISSION_TYPE:= uTRANSMISSION_TYPE;
         end if;
 
-        if vNO_OF_SEATS is NULL or  LENGTH(vNO_OF_SEATS) = 0 then
+        if uNO_OF_SEATS is NULL or  LENGTH(uNO_OF_SEATS) = 0 then
             Select NO_OF_SEATS into vNO_OF_SEATS from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vNO_OF_SEATS:= uNO_OF_SEATS;
         end if;
 
-        if vREGISTRATION_DATE is NULL or to_char(LENGTH(vREGISTRATION_DATE)) is NULL then
+        if uREGISTRATION_DATE is NULL or to_char(LENGTH(uREGISTRATION_DATE)) is NULL then
                 Select REGISTRATION_DATE into vREGISTRATION_DATE from CAR_REGISTRATION where CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+        else
+            vREGISTRATION_DATE:= uREGISTRATION_DATE;
         end if;
-
+        
         Select BODY_CAR_REGISTER_CHECK(vCAR_REGISTER_ID, vCAR_NAME, vCAR_COLOR, vCAR_COMPANY, vYEAR_OF_MANUFACTURE, vREGISTRATION_DATE, vFUEL_TYPE, vTRANSMISSION_TYPE, vNO_OF_SEATS, vCAR_CATEGORY_ID, vEMPLOYEE_ID, vADDRESS_ID) INTO Conditions from dual;
         if Conditions = 'YES' and  temp_car_id > 0 THEN
             UPDATE CAR_REGISTRATION SET CAR_NAME = UPPER(vCAR_NAME),  
@@ -375,11 +486,20 @@ create or replace PACKAGE BODY PKG_CAR_REGISTER AS
             CAR_CATEGORY_ID = UPPER(vCAR_CATEGORY_ID),
             EMPLOYEE_ID = UPPER(vEMPLOYEE_ID),
             ADDRESS_ID = UPPER(vADDRESS_ID)
-            WHERE vCAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
+            WHERE CAR_REGISTER_ID = UPPER(vCAR_REGISTER_ID);
             dbms_output.put_line('Data updated successfully !!!');
         ELSE
             dbms_output.put_line('Invalid CAR_REGISTER ID !!!');
          end if;
+    EXCEPTION
+      when INVALID_EMPLOYEE_ID then
+        dbms_output.put_line('Invalid employee email !!!');
+      when INVALID_CAR_REGISTER_ID then
+        dbms_output.put_line('Invalid CAR_REGISTER_ID !!!');
     END BODY_CAR_REGISTER_UPDATE_TABLE;
 
 END PKG_CAR_REGISTER;
+
+-- select * from car_registration;
+-- EXECUTE PKG_CAR_REGISTER.BODY_CAR_REGISTER_UPDATE_TABLE('Artyhbnju123fthyg1', 'zica', '', '', '', '1-JAN-2019', 'gasoline','manual',4,'SEDAN','PAGOLU.S@NORTHEASTERN.EDU');
+-- EXECUTE PKG_CAR_REGISTER.BODY_CAR_REGISTER_DELETE_DATA('Artyhbnju123fthyg1');
