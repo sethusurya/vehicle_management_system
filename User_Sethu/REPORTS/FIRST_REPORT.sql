fce1ae253d29cd32dac768b96b5c0714941f601c
sethu <pagolu.s@northeastern.edu>
2022-04-28 19:25:38 -0400

    add parking spot wrt zipcode reports

--- User_Sethu/REPORTS/FIRST_REPORT.sql
+++ User_Sethu/REPORTS/FIRST_REPORT.sql
@@ -1,2 +1,13 @@
 select EMPLOYEE_ID, 'CAR_REGISTRATION_STATUS', COUNT(CAR_REGISTER_ID)
 from CAR_REGISTRATION group by EMPLOYEE_ID;
+
+
+--------------------------------------------------------------------------
+
+select ZIPCODE,'No.OF ADDRESS FOR PARKING', count(*) from address
+where address_type='PARKING'
+group by zipcode;
+
+
+-----------------------------------------------------------------------
+
