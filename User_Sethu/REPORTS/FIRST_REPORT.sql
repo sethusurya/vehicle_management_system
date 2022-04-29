00aa64abd03a6fcfdad9634fad057689790332d7
sethu <pagolu.s@northeastern.edu>
2022-04-28 22:38:59 -0400

    add revenue based on category type

--- User_Sethu/REPORTS/FIRST_REPORT.sql
+++ User_Sethu/REPORTS/FIRST_REPORT.sql
@@ -11,3 +11,15 @@ group by zipcode;
 
 -----------------------------------------------------------------------
 
+select cc.car_category_name,'REVENUE', sum(t.TOTAL_COST)from transaction t
+LEFT join BOOKING b
+ON t.transaction_id = b.transaction_id
+lefT join car_listing Cl
+on cl.listing_id = b.listing_id
+LEFT JOIN CAR_REGISTRATION CR
+ON cr.car_register_id = cl.car_register_id
+left join car_category cc
+on cr.car_category_id = cc.car_category_id
+GROUP BY cc.car_category_name
+
+--------------------------------------------------------------------------
