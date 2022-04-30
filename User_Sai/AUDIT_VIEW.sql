ced417d4f3c4e6333e7247caeeea77150d49c147
SaiswarnaR <90537859+SaiswarnaR@users.noreply.github.com>
2022-04-29 21:35:19 -0400

    Changes on Audit View

--- User_Sai/AUDIT_VIEW.sql
+++ User_Sai/AUDIT_VIEW.sql
@@ -4,11 +4,8 @@
 ------------------------------------
 
 create or replace view audit_view as
-select au_trans.USERNAME, au_trans.TABLE_NAME, au_trans.AUDIT_DATE,au_trans.ACTION from AUDIT_DATA_TRANSACTION au_trans;
-select au_usr.USERNAME, au_usr.TABLE_NAME, au_usr.AUDIT_DATE, au_usr.ACTION from AUDIT_DATA_USER au_usr;
-select au_adr.USERNAME, au_adr.TABLE_NAME, au_adr.AUDIT_DATE, au_adr.ACTION from AUDIT_DATA_ADDRESS au_adr;
-select au_book.USERNAME, au_book.TABLE_NAME, au_book.AUDIT_DATE, au_book.ACTION from AUDIT_DATA_BOOKING au_book;
-select au_carlist.USERNAME, au_carlist.TABLE_NAME, au_carlist.AUDIT_DATE, au_carlist.ACTION from AUDIT_DATA_CAR_LISTING au_carlist;
-select au_car.USERNAME, au_car.TABLE_NAME, au_car.AUDIT_DATE, au_car.ACTION from AUDIT_DATA_CAR_REGISTRATION au_car;
-select au_emp.USERNAME, au_emp.TABLE_NAME, au_emp.AUDIT_DATE, au_emp.ACTION from AUDIT_DATA_EMPLOYEE au_emp;
-select au_role.USERNAME, au_role.TABLE_NAME, au_role.AUDIT_DATE, au_role.ACTION from AUDIT_DATA_ROLES au_role;
+select 
+au_usr.USERNAME, au_usr.TABLE_NAME as AUDIT_USER_TABLE, au_usr.AUDIT_DATE as USER_AUDIT_DATE, au_usr.ACTION as USER_ACTION,
+au_trans.TABLE_NAME as AUDIT_TRANSACTION_TABLE, au_trans.AUDIT_DATE as TRANSACTION_AUDIT,au_trans.ACTION as TRANSACTION_TABLE_ACTION
+from AUDIT_DATA_TRANSACTION au_trans
+join AUDIT_DATA_USER au_usr on au_usr.AUDIT_ID = au_trans.AUDIT_ID;
